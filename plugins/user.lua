392750b9f8b05deaae74c7f39fb4a8c0ba645749
to19960425 <t.o19960425@gmail.com>
2024-11-06 13:23:33 +0900

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,14 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- install with yarn or npm
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
